235aa816ada853d25621a96292f9bcb352c9f7a3
dadao34 <47913477+dadao34@users.noreply.github.com>
2020-04-04 16:22:11 +0200

    Update lucas.cpp

--- lucas.cpp
+++ lucas.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
+void ClearScreen()
+{
+	HANDLE                     hStdOut;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD                      count;
+	DWORD                      cellCount;
+	COORD                      homeCoords = { 0, 0 };
 
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+	/* Get the number of cells in the current buffer */
+	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+	cellCount = csbi.dwSize.X * csbi.dwSize.Y;
+
+	/* Fill the entire buffer with spaces */
+	if (!FillConsoleOutputCharacter(
+		hStdOut,
+		(TCHAR)' ',
+		cellCount,
+		homeCoords,
+		&count
+		)) return;
+
+	/* Fill the entire buffer with the current colors and attributes */
+	if (!FillConsoleOutputAttribute(
+		hStdOut,
+		csbi.wAttributes,
+		cellCount,
+		homeCoords,
+		&count
+		)) return;
+
+	/* Move the cursor home */
+	SetConsoleCursorPosition(hStdOut, homeCoords);
+}
 
 void affPlateau(int v[24])
 {
@@ -45,6 +81,7 @@ int placePion(int tab[24], int* tour)
 	if (tab[dm] == 0)
 	{
 		tab[dm] = *tour;
+		ClearScreen();
 	}
 	else { cout << "La case choisie n'est pas disponible" << endl; placePion(tab, tour); }
 	return dm;
@@ -60,34 +97,34 @@ bool check_moulin(int tab[24], int* tour, int* dm)//dm: dernier mouvement
 		if (*dm % 8 == 0)
 		{
 			moulin = ((tab[*dm + 1]) == *tour) && (tab[*dm + 7] == *tour);
-			if (moulin) 
+			if (moulin)
 			{
-				return moulin; 
+				return moulin;
 			}
 		}
 		//Sur les carrés(pour 0,8,16), si vrai on retourne le résultat immédiatement
 		else
 		{
-			moulin = (tab[*dm + 1]) == *tour && (tab[*dm - 1] == *tour); 
-			if (moulin) 
-			{ 
-				return moulin; 
+			moulin = (tab[*dm + 1]) == *tour && (tab[*dm - 1] == *tour);
+			if (moulin)
+			{
+				return moulin;
 			}
 		}
 		//Sur les carrés(sauf 0,8,16 qui posent problème), si vrai on retourne le résultat immédiatement
-		if (*dm < 8) 
-		{ 
+		if (*dm < 8)
+		{
 			moulin = (tab[*dm + 8]) == *tour && (tab[*dm + 16] == *tour);
 		}
 		//Entre les carrés(carré externe)
-		if (*dm >= 8 && *dm < 16) 
+		if (*dm >= 8 && *dm < 16)
 		{
-			moulin = (tab[*dm - 8]) == *tour && (tab[*dm + 8] == *tour); 
+			moulin = (tab[*dm - 8]) == *tour && (tab[*dm + 8] == *tour);
 		}
 		//Entre les carrés(carré milieu)	
-		if (*dm >= 16) 
-		{ 
-			moulin = (tab[*dm - 8]) == *tour && (tab[*dm - 16] == *tour); 
+		if (*dm >= 16)
+		{
+			moulin = (tab[*dm - 8]) == *tour && (tab[*dm - 16] == *tour);
 		}
 		//Entre les carrés(carré interne)
 
@@ -98,29 +135,29 @@ bool check_moulin(int tab[24], int* tour, int* dm)//dm: dernier mouvement
 		if (*dm % 8 == 1)
 		{
 			moulin = (tab[*dm - 1]) == *tour && (tab[*dm + 6] == *tour);
-			if (moulin) 
+			if (moulin)
 			{
-				return moulin; 
+				return moulin;
 			}
 		}
 		//pour 1,9 et 17 (indices précédents), si vrai on retourne le résultat immédiatement
 		else
 		{
 			moulin = (tab[*dm - 1]) == *tour && (tab[*dm - 2] == *tour);
-			if (moulin) 
+			if (moulin)
 			{
-				return moulin; 
+				return moulin;
 			}
 		}
 		//pour les autres valeurs (indices précédents), si vrai on retourne le résultat immédiatement
-		if (*dm % 8 == 7) 
-		{ 
-			moulin = (tab[*dm - 7]) == *tour && (tab[*dm - 6] == *tour); 
+		if (*dm % 8 == 7)
+		{
+			moulin = (tab[*dm - 7]) == *tour && (tab[*dm - 6] == *tour);
 		}
 		//pour 7,15 et 23 (indices suivants)
-		else 
+		else
 		{
-			moulin = (tab[*dm + 1]) == *tour && (tab[*dm + 2] == *tour); 
+			moulin = (tab[*dm + 1]) == *tour && (tab[*dm + 2] == *tour);
 		}
 		//pour les autres valeurs (indices suivants)
 	}
@@ -133,76 +170,245 @@ bool MoulinPartout(int tab[24], int* autreJ)
 	int cpt = 0;
 	while (condition && cpt < 24)
 	{
-		condition = check_moulin(tab, autreJ, &cpt);
+		if (tab[cpt] == *autreJ)
+		{
+			condition = check_moulin(tab, autreJ, &cpt);
+		}
 		cpt++;
 	}
 	return condition;
 }
 
-void supprPion(int tab[24], int* tour)
+void supprPion(int tab[24], int* tour, int* pionJ1, int* pionJ2)
 {
 	int autreJ;
 	cout << "Joueur " << *tour << ",entrez le numero du pion entre 0 et 23 que vous voulez supprimer : ";
-	int valsup = demandeVal();
+	int valsuppr = demandeVal();
 	{
-		if ((tab[valsup] != 0) && (tab[valsup] != *tour))
+		if ((tab[valsuppr] != 0) && (tab[valsuppr] != *tour))
 		{
 			if (*tour == 1)
-			{				
+			{
 				autreJ = 2;
 				if (MoulinPartout(tab, &autreJ))
 				{
-					cout << "suprimez le pion de votre choix" << endl;
-					tab[valsup] = 0;
+					//cout << "suprimez le pion de votre choix" << endl;
+					tab[valsuppr] = 0;
 				}
-				else if (check_moulin(tab, &autreJ, &valsup))
+				else if (check_moulin(tab, &autreJ, &valsuppr))
 				{
 					cout << "Ce pion fait parti d'un moulin !" << endl;
-					supprPion(tab, tour);
+					supprPion(tab, tour, pionJ1, pionJ2);
 				}
 				else
 				{
-					tab[valsup] = 0;
+					tab[valsuppr] = 0;
 				}
+				(*pionJ2) --;
 			}
 			if (*tour == 2)
 			{
 				autreJ = 1;
-				if (MoulinPartout (tab, &autreJ))
+				if (MoulinPartout(tab, &autreJ))
 				{
-					cout << "suprimez le pion de votre choix" << endl;
-					tab[valsup] = 0;
+					//cout << "suprimez le pion de votre choix" << endl;
+					tab[valsuppr] = 0;
 				}
-				else if (check_moulin(tab, &autreJ, &valsup))
+				else if (check_moulin(tab, &autreJ, &valsuppr))
 				{
 					cout << "Ce pion fait parti d'un moulin !" << endl;
-					supprPion(tab, tour);
+					supprPion(tab, tour, pionJ1, pionJ2);
 				}
 				else
 				{
-					tab[valsup] = 0;
+					tab[valsuppr] = 0;
 				}
+				(*pionJ1) --;
 			}
 		}
-		else 
-		{ 
-			cout << "Vous ne pouvez pas supprimer cette case" << endl; supprPion(tab, tour); 
+		else
+		{
+			cout << "Vous ne pouvez pas supprimer cette case" << endl; supprPion(tab, tour, pionJ1, pionJ2);
 		}
 		affPlateau(tab);
 	}
+
 }
 
-void phase1(int tab[24], int* tour, int* dm) 
+bool checkMove(int tab[24], int* tour, int proMove, int sPion)
 {
+	bool move = false;
+	// impairs
+	if (sPion % 2 == 1)
+	{
+		// 7 15 23
+		if (sPion % 8 == 7)
+		{
+			move = (sPion - 7 == proMove || sPion - 1 == proMove);
+		}
+		else
+		{
+			move = (sPion + 1 == proMove || sPion - 1 == proMove);
+		}
+	}
+
+	// pairs
+	else if (sPion % 2 == 0)
+	{
+		if (sPion % 2 == 0)
+		{
+
+			if (sPion % 8 == 0)
+			{
+				move = (sPion + 1 == proMove) || (sPion + 7 == proMove);
+			} //Sur les carrés(pour 0,8,16), si vrai on retourne le résultat immédiatement
+			else
+
+			{
+				move = (sPion + 1 == proMove) || (sPion - 1 == proMove);
+			}
+			//Sur les carrés(sauf 0,8,16 qui posent problème), si vrai on retourne le résultat immédiatement
+
+			if (sPion < 8)
+			{
+				move = (sPion + 8 == proMove);
+			}
+			//Entre les carrés(carré externe)
+
+			if (sPion >= 8 && sPion < 16)
+			{
+				move = (sPion - 8 == proMove) || (sPion + 8 == proMove);
+			}
+			//Entre les carrés(carré milieu)
+
+			if (sPion >= 16)
+			{
+				move = (sPion - 8 == proMove);
+			}
+			//Entre les carrés(carré interne)
+
+		}
+		return move;
+	}
+}
+
+bool checkBlocPartout (int tab[24], int* tour, int sPion)
+{
+	bool move = false;
+	// impairs
+	if (sPion % 2 == 1)
+	{
+		// 7 15 23
+		if (sPion % 8 == 7)
+		{
+			move = (tab[sPion - 7] == 0 || tab[sPion - 1] == 0);
+		}
+		else
+		{
+			move = (tab[sPion + 1] == 0 || tab[sPion - 1] == 0);
+		}
+	}
+
+	// pairs
+	else if (sPion % 2 == 0)
+	{
+		if (sPion % 2 == 0)
+		{
+
+			if (sPion % 8 == 0)
+			{
+				move = (tab[sPion + 1] == 0) || (tab[sPion + 7] == 0);
+			} //Sur les carrés(pour 0,8,16), si vrai on retourne le résultat immédiatement
+			else
+
+			{
+				move = (tab[sPion + 1] == 0) || (tab[sPion - 1] == 0);
+			}
+			//Sur les carrés(sauf 0,8,16 qui posent problème), si vrai on retourne le résultat immédiatement
+
+			if (sPion < 8)
+			{
+				move = (tab[sPion + 8] == 0);
+			}
+			//Entre les carrés(carré externe)
+
+			if (sPion >= 8 && sPion < 16)
+			{
+				move = (tab[sPion - 8] == 0) || (tab[sPion + 8] == 0);
+			}
+			//Entre les carrés(carré milieu)
+
+			if (sPion >= 16)
+			{
+				move = (tab[sPion - 8] == 0);
+			}
+			//Entre les carrés(carré interne)
+
+		}
+		return move;
+	}
+}
+
+bool blocPartout (int tab[24], int* tour)
+{
+	{
+		bool condition = false;
+		int cpt = 0;
+		while (condition && cpt < 24)
+		{
+			if (tab[cpt] == *tour)
+			{
+				condition = !checkBlocPartout(tab, tour, cpt);
+			}
+			cpt++;
+		}
+		return condition;
+	}
+}
+
+void movePion(int tab[24], int* tour, int* dm)
+{
+	int proMove;
+	int sPion;
+	sPion = demandeVal();
+	if (tab[sPion] == *tour)
+	{
+		proMove = demandeVal();
+		if (checkMove(tab, tour, proMove, sPion))
+		{
+			*dm = proMove;
+			tab[*dm] = *tour;
+		}
+		else
+		{
+			cout << "ce mouvement est impossible, choisit une autre valeur ;)" << endl;
+			movePion(tab, tour, dm);
+
+		}
+
+	}
+}
+	
+void phase1(int tab[24], int* tour, int* dm, int* pionJ1, int* pionJ2)
+{
+	
 	for (int i = 0; i < 18; i++)
 	{
 		*dm = placePion(tab, tour);
 		affPlateau(tab);
-		if (check_moulin(tab, tour, dm)) 
-		{ 
-			supprPion(tab, tour); 
+		if (check_moulin(tab, tour, dm))
+		{
+			supprPion(tab, tour, pionJ1, pionJ2);
 		}
 		chgt_tour(tour);
+	}
+}
+
+void phase2(int tab[24], int* tour, int* dm , int* pionJ1, int* pionJ2)
+{
+	while (*pionJ1 > 3 || *pionJ2 > 3)
+	{
+
 	}
 }
 
@@ -212,7 +418,9 @@ int main()
 	// 1 si c est le tour de p1 2 si c est le tour de p2 
 	int turnP = 1;
 	int dermove;
+	int pionJ1 = 9;
+	int pionJ2 = 9;
 	affPlateau(tableau);
-	phase1(tableau, &turnP, &dermove);
+	phase1(tableau, &turnP, &dermove, &pionJ1, &pionJ2);
 	return 0;
 }
