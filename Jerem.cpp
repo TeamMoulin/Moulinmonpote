2c2e50e7b7f73c9e856b1373e20e8ed5d4944983
EpicJerem <62223964+EpicJerem@users.noreply.github.com>
2020-04-16 17:58:50 +0200

    Update Jerem.cpp

--- Jerem.cpp
+++ Jerem.cpp
@@ -1,42 +1,11 @@
 #include <iostream>
 #include <windows.h>
+#include <stdlib.h>
 using namespace std;
 
 void ClearScreen()//Nettoie le terminal quand appelé
 {
-	HANDLE                     hStdOut;
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	DWORD                      count;
-	DWORD                      cellCount;
-	COORD                      homeCoords = { 0, 0 };
-
-	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (hStdOut == INVALID_HANDLE_VALUE) return;
-
-	/* Get the number of cells in the current buffer */
-	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
-	cellCount = csbi.dwSize.X * csbi.dwSize.Y;
-
-	/* Fill the entire buffer with spaces */
-	if (!FillConsoleOutputCharacter(
-		hStdOut,
-		(TCHAR)' ',
-		cellCount,
-		homeCoords,
-		&count
-		)) return;
-
-	/* Fill the entire buffer with the current colors and attributes */
-	if (!FillConsoleOutputAttribute(
-		hStdOut,
-		csbi.wAttributes,
-		cellCount,
-		homeCoords,
-		&count
-		)) return;
-
-	/* Move the cursor home */
-	SetConsoleCursorPosition(hStdOut, homeCoords);
+	system("CLS");
 }
 
 void coutstr(string a, int nombre)//Affichage d'un caractère le nombre de fois voulu (coutstr(f,3)="fff")
